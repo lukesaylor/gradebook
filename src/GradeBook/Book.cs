7d731ec06a98f718fa332f6fed6098d995c3bc80
Stephanie Altman <slappyhoursound@gmail.com>
2020-10-19 16:06:01 -0400

    module 4

---
+++ src/GradeBook/Book.cs
@@ -0,0 +1,18 @@
+using System.Collections.Generic;
+
+namespace GradeBook
+{
+
+    class Book
+    {
+        public Book() {
+            grades = new List<double>();
+        }
+        public void Addgrade(double grade)
+        {
+            grades.Add(grade);
+        }
+
+        List<double> grades;
+    }
+}
