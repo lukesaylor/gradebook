f3cacf4c4c47c580ee07169babd4231f73376c58
Stephanie Altman <slappyhoursound@gmail.com>
2020-10-20 16:13:52 -0400

    testing

--- test/GradeBook.Tests/TypeTests.cs
+++ test/GradeBook.Tests/TypeTests.cs
@@ -11,6 +11,9 @@ namespace GradeBook.Tests
             var book1 = GetBook("Book 1");
             var book2 = GetBook("Book 2");
 
+            Assert.Equal("Book 1", book1.Name);
+            Assert.Equal("Book 2", book2.Name);
+
         }
         Book  GetBook(string name)
         {
