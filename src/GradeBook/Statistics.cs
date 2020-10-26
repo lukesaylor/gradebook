4c5086a8b83c9b22542fd17a348672fae0497ecd
Stephanie Altman <slappyhoursound@gmail.com>
2020-10-26 16:05:37 -0400

    refactoring statistics

--- src/GradeBook/Statistics.cs
+++ src/GradeBook/Statistics.cs
@@ -1,9 +1,57 @@
+using System;
+
 namespace GradeBook
 {
-    public class Statistics {
-        public double Average;
-        public double High;
+    public class Statistics
+    {
+        public double Average
+        {
+            get
+            {
+                return Sum / Count;
+            }
+        }
+        public double High; //unit 10 refactoring statistics
         public double Low;
-        public char Letter;
+        public char Letter
+        {
+            switch (result.Average)
+            {
+                case var d when d >= 90.0:
+                    result.Letter = 'A';
+                    break;
+                case var d when d >= 80.0:
+                    result.Letter = 'B';
+                    break;
+                case var d when d >= 70.0:
+                    result.Letter = 'C';
+                    break;
+                case var d when d >= 60.0:
+                    result.Letter = 'D';
+                    break;
+                default:
+                    result.Letter = 'F';
+                    break;
+            }
+        }
+        public double Sum;
+        public int Count;
+
+        public void Add(double number)
+        {
+            Sum += number;
+            Count += 1;
+            Low = Math.Min(number, Low);
+            High = Math.Max(number, High);
+        }
+
+        public Statistics(double average, double high, double low, char letter)
+        {
+             
+            Count = 0;
+            Sum = 0.0;
+            High = double.MinValue;
+            Low = double.MaxValue;
+        }
     }
 }
